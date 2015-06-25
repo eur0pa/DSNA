759524be99ef1af2a505d626a8b211b89e159a27
europa <europa@alessandro.pw>
2015-06-25 12:26:14 +0200

    Update Matchmaking.h

--- DSNA/Steam/Matchmaking.h
+++ DSNA/Steam/Matchmaking.h
@@ -2,8 +2,6 @@
 #include "stdafx.h"
 #include "Libs\steam\steam_api.h"
 
-//typedef void(__thiscall* AddRequestLobbyListDistanceFilter)(void*, ELobbyDistanceFilter);   // vft[9]
-typedef void(__stdcall *AddRequestLobbyListDistanceFilter)(ELobbyDistanceFilter);
+typedef void(__stdcall *AddRequestLobbyListDistanceFilter)(ELobbyDistanceFilter);   // vft[9]
 extern AddRequestLobbyListDistanceFilter oAddRequestLobbyListDistanceFilter;
 extern void __stdcall tAddRequestLobbyListDistanceFilter(ELobbyDistanceFilter);
-//extern void __fastcall tAddRequestLobbyListDistanceFilter(void*, void*, ELobbyDistanceFilter);
